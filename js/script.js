8188245e88981adf71ec0a58603cb72fe959b8ac
imekenye <ismekenye@gmail.com>
2018-12-09 14:09:54 -0800

    update/add javascript functions & methods

---
+++ js/script.js
@@ -0,0 +1,45 @@
+var quizResults = function(numOne, numTwo ,numThree, numFour, numFive){
+    return numOne + numTwo + numThree + numFour + numFive;
+
+
+};
+
+
+$(document).ready(function(){
+
+$("form#quest").submit(function(event){
+  var questOne = parseInt($("input:radio[name=Q1]:checked").val());
+   if($("input:radio[name=quest1]:checked").length == 0){
+     $('#err1').text("Please select one");
+             return false;
+   }
+  var questTwo = parseInt($("input:radio[name=Q2]:checked").val());
+    if($("input:radio[name=quest2]:checked").length == 0){
+      $('#err2').text("Please select one");
+            return false;
+  }
+  var questThree = parseInt($("input:radio[name=Q3]:checked").val());
+    if($("input:radio[name=quest3]:checked").length == 0){
+      $('#err3').text("Please select one");
+            return false;
+  }
+  var questFour = parseInt($("input:radio[name=Q4]:checked").val());
+    if($("input:radio[name=quest4]:checked").length == 0){
+      $('#err4').text("Please select one");
+            return false;
+  }
+  var questFive = parseInt($("input:radio[name=Q5]:checked").val());
+    if($("input:radio[name=quest5]:checked").length == 0){
+      $('#err5').text("Please select one");
+            return false;
+  }
+  var finalResult = quizResults(questOne, questTwo, questThree, questFour, questFive);
+
+  $("#display").text("Your score is: " + finalResult + " /25");
+
+  event.preventDefault();
+  $("#quest").fadeOut();
+  $(".displayResult").show();
+  $("form").trigger("reset");
+});
+});
